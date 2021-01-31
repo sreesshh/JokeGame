63b3decead6ac96f863ee74ac233ad67006a1821
sasha <77569442+sreesshh@users.noreply.github.com>
2021-01-31 14:34:28 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame as pg
 
-FPS = 30
+FPS = 60
 
+class Window:
+    width = 640
+    height = 480
+    center_x = width/2
+    center_y = height/2
+    
 pg.init()
-screen = pg.display.set_mode((640, 480))
+screen = pg.display.set_mode((Window.width, Window.height))
 clock = pg.time.Clock()
 
 class Button:
@@ -18,25 +24,41 @@ class Button:
     def draw(self, screen):
         pg.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
     
-    def is_over(self):
-        pass
-    def jumpto(self):
-        pass
-    
-RED = (255, 0,0)
-WHITE = (255, 255, 255)
-btn_yes = Button(RED, 10, 10, 100, 30)
-btn_no = Button(RED, 160, 10, 100, 30)
+    def is_over(self, mouse_x, mouse_y):
+        return self.x <= mouse_x <= self.x + self.width and \
+           self.y <= mouse_y <= self.y + self.height
+          
+    def jumpto(self, x, y):
+        self.x = x
+        self.y = y
+        
+VIOLET = (204, 0,255)
+AQUA = (102, 255, 255)
+
+distance_to_center_x = 30
+Button.width = 100
+Button.height = 30
+view_x = Window.center_x - Button.width - distance_to_center_x
+view_y = Window.center_y;
+
+btn_yes = Button(VIOLET, view_x, view_y, Button.width, Button.height)
+btn_no = Button(VIOLET, view_x + Button.width + distance_to_center_x*2, view_y, Button.width, Button.height)
 
 running = True
 while running:
     clock.tick(FPS)
+    screen.fill(AQUA)
     
-    screen.fill(WHITE)
+    list_events = pg.event.get()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-            
+        if event.type == pg.MOUSEMOTION:
+            mouse_x, mouse_y = event.pos
+            print(btn_no.jumpto(mouse_x, mouse_y))
+            if btn_no.is_over(mouse_x, mouse_y):
+                btn_no.jumpto(10, 160)
+    
     btn_yes.draw(screen)
     btn_no.draw(screen)
     
